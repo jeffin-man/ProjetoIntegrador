985bedc9c07118783d99ddbb45f7a0224327343c
jeffin-man <jeffersonjunior1b@gmail.com>
2025-10-19 22:31:52 -0300

    ui

--- Assets/Scripts/MiniGameSt.cs
+++ Assets/Scripts/MiniGameSt.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 public class MiniGameSt : MonoBehaviour
 {
     private bool playerInRange = false;
+    public GameObject UI;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerInRange = true;
+            UI.SetActive(true);
         }
     }
 
@@ -19,6 +21,7 @@ public class MiniGameSt : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
+            UI.SetActive(false);
         }
     }
 
